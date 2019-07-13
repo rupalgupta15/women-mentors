6b62f8f46f4ec5a31351f7dd21df24c8ed83a123
Rupal Gupta <rupal@Rupals-MBP.fios-router.home>
2019-07-12 23:40:55 -0400

    modified route show_json

--- flaskmentor/routes.py
+++ flaskmentor/routes.py
@@ -9,7 +9,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 import requests
 
 
-@app.route('/')
+@app.route('/show')
 def show_json2():
     filename = os.path.join('data', 'final_mentors.json')
 
